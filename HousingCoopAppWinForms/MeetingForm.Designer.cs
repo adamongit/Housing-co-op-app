da3b70a08607649f16c485f009b44cc4aa8e84ad
Adam Taylor <adam.david.taylor@gmail.com>
2012-10-30 22:42:45 +0000

    Added datetimepicker functionality + check to add meeting screen

--- HousingCoopAppWinForms/MeetingForm.Designer.cs
+++ HousingCoopAppWinForms/MeetingForm.Designer.cs
@@ -32,12 +32,14 @@
             this.lblDateFrom = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtLocation = new System.Windows.Forms.TextBox();
-            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
-            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpTimeFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTimeTo = new System.Windows.Forms.DateTimePicker();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.cbSubgroup = new System.Windows.Forms.ComboBox();
+            this.dtpDateFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpDateTo = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // lblLocation
@@ -72,28 +74,34 @@
             this.txtLocation.Location = new System.Drawing.Point(159, 46);
             this.txtLocation.MaxLength = 50;
             this.txtLocation.Name = "txtLocation";
-            this.txtLocation.Size = new System.Drawing.Size(310, 20);
+            this.txtLocation.Size = new System.Drawing.Size(207, 20);
             this.txtLocation.TabIndex = 3;
             // 
-            // dtpFrom
+            // dtpTimeFrom
             // 
-            this.dtpFrom.Location = new System.Drawing.Point(159, 86);
-            this.dtpFrom.Name = "dtpFrom";
-            this.dtpFrom.Size = new System.Drawing.Size(200, 20);
-            this.dtpFrom.TabIndex = 4;
+            this.dtpTimeFrom.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTimeFrom.Location = new System.Drawing.Point(299, 89);
+            this.dtpTimeFrom.Name = "dtpTimeFrom";
+            this.dtpTimeFrom.ShowUpDown = true;
+            this.dtpTimeFrom.Size = new System.Drawing.Size(67, 20);
+            this.dtpTimeFrom.TabIndex = 4;
+            this.dtpTimeFrom.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
             // 
-            // dtpTo
+            // dtpTimeTo
             // 
-            this.dtpTo.Location = new System.Drawing.Point(159, 117);
-            this.dtpTo.Name = "dtpTo";
-            this.dtpTo.Size = new System.Drawing.Size(200, 20);
-            this.dtpTo.TabIndex = 5;
+            this.dtpTimeTo.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTimeTo.Location = new System.Drawing.Point(299, 117);
+            this.dtpTimeTo.Name = "dtpTimeTo";
+            this.dtpTimeTo.ShowUpDown = true;
+            this.dtpTimeTo.Size = new System.Drawing.Size(67, 20);
+            this.dtpTimeTo.TabIndex = 5;
+            this.dtpTimeTo.ValueChanged += new System.EventHandler(this.dtpTo_ValueChanged);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(393, 265);
+            this.btnCancel.Location = new System.Drawing.Point(269, 213);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(97, 23);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -101,9 +109,9 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(312, 265);
+            this.btnSave.Location = new System.Drawing.Point(159, 213);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.Size = new System.Drawing.Size(97, 23);
             this.btnSave.TabIndex = 7;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -120,23 +128,40 @@
             // 
             // cbSubgroup
             // 
+            this.cbSubgroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbSubgroup.FormattingEnabled = true;
             this.cbSubgroup.Location = new System.Drawing.Point(159, 152);
             this.cbSubgroup.Name = "cbSubgroup";
-            this.cbSubgroup.Size = new System.Drawing.Size(200, 21);
+            this.cbSubgroup.Size = new System.Drawing.Size(207, 21);
             this.cbSubgroup.TabIndex = 9;
+            // 
+            // dtpDateFrom
+            // 
+            this.dtpDateFrom.Location = new System.Drawing.Point(159, 89);
+            this.dtpDateFrom.Name = "dtpDateFrom";
+            this.dtpDateFrom.Size = new System.Drawing.Size(134, 20);
+            this.dtpDateFrom.TabIndex = 12;
+            // 
+            // dtpDateTo
+            // 
+            this.dtpDateTo.Location = new System.Drawing.Point(159, 117);
+            this.dtpDateTo.Name = "dtpDateTo";
+            this.dtpDateTo.Size = new System.Drawing.Size(134, 20);
+            this.dtpDateTo.TabIndex = 13;
             // 
             // MeetingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(489, 302);
+            this.ClientSize = new System.Drawing.Size(382, 256);
+            this.Controls.Add(this.dtpDateTo);
+            this.Controls.Add(this.dtpDateFrom);
             this.Controls.Add(this.cbSubgroup);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.dtpTo);
-            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.dtpTimeTo);
+            this.Controls.Add(this.dtpTimeFrom);
             this.Controls.Add(this.txtLocation);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblDateFrom);
@@ -155,11 +180,13 @@
         private System.Windows.Forms.Label lblDateFrom;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtLocation;
-        private System.Windows.Forms.DateTimePicker dtpFrom;
-        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpTimeFrom;
+        private System.Windows.Forms.DateTimePicker dtpTimeTo;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbSubgroup;
+        private System.Windows.Forms.DateTimePicker dtpDateFrom;
+        private System.Windows.Forms.DateTimePicker dtpDateTo;
     }
 }
--- HousingCoopAppWinForms/MeetingForm.cs
+++ HousingCoopAppWinForms/MeetingForm.cs
@@ -33,9 +33,60 @@ namespace HousingCoopAppWinForms
         /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void btnSave_Click(object sender, EventArgs e)
         {
-            Meeting newMeeting = new Meeting();
-            newMeeting.DateTimeFrom = this.dtpFrom.Value;
-            newMeeting.DateTimeTo = this.dtpTo.Value;
+            DateTime datetimeFrom = GetDateTimeFrom();
+            DateTime datetimeTo = GetDateTimeTo();
+
+            if (datetimeFrom >= datetimeTo)
+            {
+                MessageBox.Show("Meeting end time must be later than the meeting start time.",
+                    "Problem with input",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                // TODO
+                Meeting newMeeting = new Meeting();
+                newMeeting.DateTimeFrom = this.dtpTimeFrom.Value;
+                newMeeting.DateTimeTo = this.dtpTimeTo.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the date time to.
+        /// </summary>
+        /// <returns></returns>
+        private DateTime GetDateTimeTo()
+        {
+            DateTime dtTo =
+               DateTime.Parse(
+                   dtpDateTo.Value.ToShortDateString());
+
+            TimeSpan tsTo = TimeSpan.Parse
+            (dtpTimeTo.Value.ToLongTimeString());
+
+            dtTo = dtTo.Date + tsTo;
+
+            return dtTo;
+        }
+
+        /// <summary>
+        /// Gets the date time from.
+        /// </summary>
+        /// <returns></returns>
+        private DateTime GetDateTimeFrom()
+        {
+            DateTime dtFrom = 
+                DateTime.Parse(
+                    dtpDateFrom.Value.ToShortDateString());
+
+            TimeSpan tsFrom = TimeSpan.Parse
+            (dtpTimeFrom.Value.ToLongTimeString());
+
+            dtFrom = dtFrom.Date + tsFrom;
+
+            return dtFrom;
+
         }
 
         /// <summary>
@@ -58,6 +109,26 @@ namespace HousingCoopAppWinForms
             cbSubgroup.DataSource = new BindingSource(Subgroup.GetAll(),null);
             cbSubgroup.DisplayMember = "Value";
             cbSubgroup.ValueMember = "Key"; 
+        }
+
+        /// <summary>
+        /// Handles the ValueChanged event of the dtpFrom control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private void dtpFrom_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the ValueChanged event of the dtpTo control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        private void dtpTo_ValueChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
